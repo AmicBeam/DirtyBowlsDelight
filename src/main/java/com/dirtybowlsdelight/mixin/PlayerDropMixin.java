1eb1e9493f24e3747253105ad4cc0bcc93c2fae2
amicbeam <1425257712@qq.com>
2025-11-03 18:00:18 +0800

    iml

--- src/main/java/com/dirtybowlsdelight/mixin/PlayerDropMixin.java
+++ src/main/java/com/dirtybowlsdelight/mixin/PlayerDropMixin.java
@@ -2,7 +2,6 @@ package com.dirtybowlsdelight.mixin;
 
 import com.dirtybowlsdelight.DirtyBowls;
 import com.dirtybowlsdelight.SoupConsumptionTracker;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
